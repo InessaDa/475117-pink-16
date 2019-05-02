ca28c87b386665a019b8ce8d36a29e574e0aa496
Инесса <ztrekosa@gmail.com>
2019-05-02 13:47:54 +1000

    модуль5

--- source/js/navigation-bar.js
+++ source/js/navigation-bar.js
@@ -2,6 +2,7 @@ var menu = document.querySelector('.navigation-list');
 var closeButton = document.querySelector('.close-button__icon');
 var menuButton = document.querySelector('.menu-button__icon');
 var header = document.querySelector('.header');
+var page = document.querySelector('.page');
 
 menu.classList.add('navigation-list--hidden');
 header.classList.add('header--closed-menu');
@@ -13,6 +14,7 @@ menuButton.addEventListener("click", function (_event) {
   header.classList.remove('header--closed-menu');
   menuButton.classList.add('menu-button__icon--hidden');
   closeButton.classList.remove('close-button__icon--hidden');
+  page.classList.add('page--padding');
 });
 
 closeButton.addEventListener("click", function(_event) {
@@ -20,4 +22,5 @@ closeButton.addEventListener("click", function(_event) {
   header.classList.add('header--closed-menu');
   closeButton.classList.add('close-button__icon--hidden');
   menuButton.classList.remove('menu-button__icon--hidden');
+  page.classList.remove('page--padding');
 })
